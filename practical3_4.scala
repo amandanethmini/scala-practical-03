7565921e97f99a0d0f0f6cb535685bc74fd642c4
amandanethmini <140165817+amandanethmini@users.noreply.github.com>
2024-06-30 21:58:53 -0700

    Update practical3_4.scala

--- practical3_4.scala
+++ practical3_4.scala
@@ -1,11 +1,19 @@
 object practical3_4 {
-  def sum(x: Array[Int]) = x match {
-    case x if (x % 2 == 0) => x + sum(x - 1)
+  def sumEvenNumbers(numbers: List[Int]): Int = {
+    numbers match {
+      case Nil => 0
+      case head :: tail =>
+        if (head % 2 == 0)
+          head + sumEvenNumbers(tail)
+        else
+          sumEvenNumbers(tail)
+    }
   }
 
   def main(args: Array[String]): Unit = {
-    val list = Array(1, 2, 3, 4, 5, 6, 7)
-    val out = sum(list)
-    println("Sum pf even numbers $out")
+    val originalList = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val sumOfEvens = sumEvenNumbers(originalList)
+    println(s"Original List: $originalList")
+    println(s"Sum of Even Numbers: $sumOfEvens")
   }
 }
