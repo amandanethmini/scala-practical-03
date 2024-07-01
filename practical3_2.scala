b31c885aeb9850f25e6fc42ed9357b01ae93f38e
amandanethmini <140165817+amandanethmini@users.noreply.github.com>
2024-07-01 07:02:16 -0700

    Update practical3_2.scala

--- practical3_2.scala
+++ practical3_2.scala
@@ -1,18 +1,18 @@
 object practical3_2 {
-  def filterLongStrings(strings: List[String]): List[String] = {
+  def filter(strings: List[String]): List[String] = {
     strings match {
       case Nil => Nil
       case head :: tail =>
-        if (head.length > 5) head :: filterLongStrings(tail)
-        else filterLongStrings(tail)
+        if (head.length > 5) head :: filter(tail)
+        else filter(tail)
     }
   }
 
   def main(args: Array[String]): Unit = {
     val originalList =
       List("apple", "banana", "cherry", "date", "elderberry", "fig", "grape")
-    val filteredList = filterLongStrings(originalList)
-    println(s"Original List: $originalList")
-    println(s"Filtered List: $filteredList")
+    val filteredList = filter(originalList)
+    println(f"Original List: $originalList")
+    println(f"Filtered List: $filteredList")
   }
 }
